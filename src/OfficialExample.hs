99d8d9d68a75d604dd54ba516980c0a26246549c
Marek Dudek <marek.dudek@gmail.com>
2017-09-20 11:19:30 +0200

    official example works

--- src/OfficialExample.hs
+++ src/OfficialExample.hs
@@ -1,8 +1,17 @@
-{-# LANGUAGE FlexibleContexts                  #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module OfficialExample where
 
 import Text.Parsec
 
+parenSet :: Stream s m Char => ParsecT s u m Char
+parenSet = char '(' >> many parenSet >> char ')'
 
---parenSet = char '(' >> many parenSet >> char ')'
+parens :: Stream s m Char => ParsecT s u m ()
+parens = (many parenSet >> eof) <|> eof
+
+t1 = parse parens "" "()" == Right ()
+t2 = parse parens "" "()(())" == Right ()
+--t3 = parse parens "" "("
+
+official_example_ok = t1 && t2
