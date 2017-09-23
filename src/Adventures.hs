37d40b06acc98692b79c74ebabbc12b88da7ee30
Marek Dudek <marek.dudek@gmail.com>
2017-09-23 18:35:16 +0200

    entire memory region parsed

--- src/Adventures.hs
+++ src/Adventures.hs
@@ -24,7 +24,7 @@ data Perms = Perms {
 } deriving (Eq, Show)
 
 data Device = Device { major:: Integer, minor :: Integer}
-  deriving Show
+  deriving (Eq, Show)
 
 data MemRegion = MemRegion {
     address  :: Address,
@@ -33,18 +33,17 @@ data MemRegion = MemRegion {
     device   :: Device,
     inode    :: Integer,
     pathname :: String 
-} deriving Show
+} deriving (Eq, Show)
 
 
 parseAddress :: Stream s m Char => ParsecT s u m Address
-parseAddress = let
-    hexStr2Int = Prelude.read . ("0x" ++)
+parseAddress = 
+  let hexStr2Int = Prelude.read . ("0x" ++)
   in do
     start <- many1 hexDigit
     char '-'
     end <- many1 hexDigit
     return $ Address (hexStr2Int start) (hexStr2Int end)
-
 
 parsePerms :: Stream s m Char => ParsecT s u m Perms
 parsePerms = 
@@ -57,3 +56,30 @@ parsePerms =
     x <- anyChar
     a <- anyChar
     return $ Perms (r =='r') (w == 'w') (x == 'x') (cA a)
+
+parseDevice :: Stream s m Char => ParsecT s u m Device
+parseDevice = 
+  let hexStr2Int = Prelude.read . ("0x" ++)
+  in do
+    maj <- many1 digit
+    char ':'
+    min <- many1 digit
+    return $ Device (hexStr2Int maj) (hexStr2Int min)
+
+parseRegion :: Stream s m Char => ParsecT s u m MemRegion
+parseRegion =
+  let hexStr2Int = Prelude.read . ("0x" ++)
+      parsePath = (many1 $ char ' ') >> (many1 $ anyChar)
+  in do
+    addr <- parseAddress
+    char ' '
+    perm <- parsePerms
+    char ' '
+    off <- many1 hexDigit
+    char ' '
+    dev <- parseDevice
+    char ' '
+    ino <- many1 digit
+    char ' '
+    pat <- parsePath <|> string ""
+    return MemRegion {address = addr, perms = perm, offset = hexStr2Int off, device = dev, inode = Prelude.read ino, pathname = pat}
--- test/AdventuresSpec.hs
+++ test/AdventuresSpec.hs
@@ -17,3 +17,18 @@ spec =
         describe "Parsing permissions" $ 
             it "works in only case" $
                 parse parsePerms "" "r-xp" `shouldBe` Right Perms {read = True, write = False, executable = True, access = Private}
+        describe "Device parsing" $ do
+            it "works on simple case" $
+                parse parseDevice "" "0:1" `shouldBe` Right Device {major = 0, minor = 1}
+            it "works on real case" $
+                parse parseDevice "" "03:0c" `shouldBe` Right Device {major = 3, minor = 0} -- TODO: this is wrong minor
+        describe "Memory region parsing" $ 
+            it "works on example" $
+                parse parseRegion "" "08058000-0805b000 rwxp 00000000 00:00 0 " `shouldBe` Right region
+                where region = MemRegion {address = a, perms = p, offset = o, device = d, inode = i, pathname = pn}
+                      a = Address {start = 134578176, end = 134590464}
+                      p = Perms {read = True, write = True, executable = True, access = Private}
+                      o = 0
+                      d = Device {major = 0, minor = 0}
+                      i = 0
+                      pn = ""
